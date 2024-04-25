8ac88520acc77a5767d2ba9474eddb3090985dcb
Mahmudul hasan <157973507+mahmudul-hasan-hridoy@users.noreply.github.com>
2024-04-25 04:15:44 +0000

    Modify

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import supabase from "@/utils/supabase";
+import supabase from "@/lib/supabase";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
