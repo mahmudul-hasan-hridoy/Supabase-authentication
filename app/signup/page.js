599e1284ea11b5e8ce6957dc132d7969409ba3db
Mahmudul hasan <157973507+mahmudul-hasan-hridoy@users.noreply.github.com>
2024-04-30 06:50:35 +0000

    Modify

--- app/signup/page.js
+++ app/signup/page.js
@@ -16,7 +16,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useToast } from "@/components/ui/use-toast";
-import { Loader, Eye, EyeOff } from "lucide-react";
+import { Loader, Eye, EyeOff, Github } from "lucide-react";
 
 export default function Signup() {
   const [isLoading, setIsLoading] = useState(false);
@@ -27,7 +27,6 @@ export default function Signup() {
   const router = useRouter();
   const { toast } = useToast();
 
-  
   const handleSignup = async (e) => {
     e.preventDefault();
     setIsLoading(true);
@@ -103,6 +102,7 @@ export default function Signup() {
   const handleTogglePassword = () => {
     setShowPassword(!showPassword);
   };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-black py-12 px-4 sm:px-6 lg:px-8">
       <Card className="max-w-md w-full">
@@ -127,7 +127,7 @@ export default function Signup() {
               variant="outline"
               className="w-full"
             >
-              <GithubIcon className="mr-2" />
+              <Github className="mr-2" />
               Sign up with GitHub
             </Button>
           </div>
@@ -183,11 +183,9 @@ export default function Signup() {
                   </button>
                 </div>
               </div>
-
             </div>
             <Button
               type="submit"
-              onClick={handleSignup}
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               disabled={isLoading}
             >
@@ -212,25 +210,6 @@ export default function Signup() {
   );
 }
 
-function GithubIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-      <path d="M9 18c-4.51 2-5-2-7-2" />
-    </svg>
-  );
-}
 
 function GoogleIcon(props) {
   return (
